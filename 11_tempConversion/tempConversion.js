03aa6aa60c7fca6387611631902ab73f69da1bc2
RyryZA <134719754+RyryZA@users.noreply.github.com>
2025-06-04 17:21:24 +0200

    Completed lesson 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+let newValue = 0;
+
+    newValue = ((f - 32)*5/9);
+    let rounded = newValue.toFixed(1);
+    let num = Number(rounded);
+
+  return num;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let newValue = 0;
+
+    newValue = (c * 9/5 + 32);
+    let rounded = newValue.toFixed(1);
+    let num = Number(rounded);
+
+  return num;
+
 };
 
 // Do not edit below this line
