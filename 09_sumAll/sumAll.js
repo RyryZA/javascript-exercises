60234e57e2466f3443cf13869e66b4cc42671617
RyryZA <134719754+RyryZA@users.noreply.github.com>
2025-06-04 15:54:28 +0200

    Completed Lesson 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+
+const sumAll = function(first, last) {
+    if (first <= 0 || last <= 0) return "ERROR";
+    if (!Number.isInteger(first) || !Number.isInteger(last)) return "ERROR";
+    if (first > last) {
+        const large = first;
+        first = last;
+        last = large;
+    }
+
+    let newAnswer = 0;
+
+    for (let i = first; i <= last; i++) {
+        newAnswer += i;
+    }
+
+    return newAnswer;
 
 };
 
